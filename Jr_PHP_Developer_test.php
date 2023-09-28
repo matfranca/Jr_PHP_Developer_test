7279ad2157c85bb97fe7153deee7f456fac12842
Matheus <math.francas@gmail.com>
2023-09-28 10:24:04 -0300

    Loops structures challenges solved

--- Jr_PHP_Developer_test.php
+++ Jr_PHP_Developer_test.php
@@ -1,3 +1,29 @@
 <?php
 
+// Countdown challenge
+echo "Countdown challenge: \n";
+
+for ($i = 10; $i > 0; $i--)
+  echo $i ."\n";
+
+////////////////////////////////////////////////////////////////////////////////
+
+//Multiplication table challenge
+echo "\nMultiplication table challenge: \n";
+
+$number = readline("Input the number:");
+
+for ($i = 1; $i <= 10; $i++)
+  echo $number ." x " . $i . " = " . $number*$i . "\n";
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Even numbers challenge
+echo "\nEven numbers challenge: \n";
+echo "The even numbers between 1 and 20\n";
+
+for ($i = 1; $i <= 20; $i++)
+  if ($i % 2 == 0)
+    echo $i ."\n";
+
 ?>
