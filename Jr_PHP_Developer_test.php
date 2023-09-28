cb263b9e8ebf86b0ecfe9fc9b7817f85ff3df858
Matheus <math.francas@gmail.com>
2023-09-28 10:55:59 -0300

    Object Oriented Programming challenges solved

--- Jr_PHP_Developer_test.php
+++ Jr_PHP_Developer_test.php
@@ -1,3 +1,34 @@
 <?php
 
+class Person {
+
+  // property declaration
+  // firstName string for storing the person's first name);
+  public $firstName;
+  // lastName (for storing the person's last name);
+  public $lastName;
+  // Date Of Birth (storing the person's date of birth in the format "YYYY-MM-DD");
+  public $dob;
+  
+  function __construct($firstName, $lastName, $dob) {
+    $this->firstName = $firstName;
+    $this->lastName = $lastName;
+    $this->dob = $dob;
+  }
+
+  function getAge($dob){
+    $dobDate = new DateTime($dob);
+    $currentDate = new DateTime();
+    
+    $age = $dobDate->diff($currentDate);
+
+    return $age->y;
+  }
+}
+
+$person = new Person("Alice", "Johnson", "1995/08/21");
+
+$age = $person->getAge($person->dob);
+echo "Age: $age years";
+
 ?>
